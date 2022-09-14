9262d04fc409a864ee9bcb11afab853a898c4c44
Jonas Hvid <mail@johv.dk>
2022-09-14 23:06:08 +0200

    Add command to insert new ID at point

--- zettlr-mode.el
+++ zettlr-mode.el
@@ -4,6 +4,9 @@
 
 (defconst zt--keywords `((,zt--id-regexp 0 'link t)))
 
+(defun zt--generate-id ()
+  (format-time-string "%Y%m%dT%H%M%S"))
+
 (defun zt--enable-minor-mode ()
   (font-lock-add-keywords nil zt--keywords)
   (font-lock-fontify-buffer))
@@ -12,16 +15,22 @@
   (font-lock-remove-keywords nil zt--keywords)
   (font-lock-fontify-buffer))
 
-(defun zt--open-at-point ()
+(defun zt-open-at-point ()
   (interactive)
   (message "open at point"))
 
+(defun zt-insert-new-id ()
+  (interactive)
+  (insert (zt--generate-id)))
+
 (defconst zt--keymap
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c C-o") 'zt--open-at-point)
+    (define-key map (kbd "C-c C-o") 'zt-open-at-point)
+    (define-key map (kbd "C-c C-t") 'zt-insert-new-id)
     map))
 
 (define-minor-mode zt-minor-mode "zt"
   :lighter " zt"
   :keymap zt--keymap
   (if zt-minor-mode (zt--enable-minor-mode) (zt--disable-minor-mode)))
+
