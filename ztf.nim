c482493fef78b6e64ba482d1cb3983ab23336beb
Jonas Hvid <mail@johv.dk>
2022-09-15 17:37:11 +0200

    Add special handling for titles of Markdown files

--- ztf.nim
+++ ztf.nim
@@ -1,6 +1,7 @@
 import std/os
 import std/nre
 import std/sugar
+import std/strutils
 
 var dir = commandLineParams()[0]
 
@@ -14,7 +15,22 @@ proc matchId(s: string): Option[string] =
   s.match(re"\d{8}T\d{6}").map(match)
 
 proc fileTitle(path: string): string =
-  path.readLines(1)[0]
+  if path.endsWith(".md"):
+    let content = path.readFile
+
+    let h1 = content.find(re"# (.+)")
+    if h1.isSome:
+      return h1.get.captures[0]
+
+    let title = content.find(re"title: (.+)(?s:.)+?---")
+    if title.isSome:
+      return title.get.captures[0]
+
+    let afterFrontmatter = content.find(re"---\n(?s:\s)+(.+)")
+    if afterFrontmatter.isSome:
+      return afterFrontmatter.get.captures[0]
+
+  return path.readLines(1)[0]
 
 let allIds = collect:
   for file in walkDir(dir):
