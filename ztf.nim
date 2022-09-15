1d7f702e82451edb99cb4838cb34e4a994a4740e
Jonas Hvid <mail@johv.dk>
2022-09-15 17:19:37 +0200

    Allow optionally searching for string

--- ztf.nim
+++ ztf.nim
@@ -4,6 +4,12 @@ import std/sugar
 
 var dir = commandLineParams()[0]
 
+var searchTerm =
+  if commandLineParams().len >= 2:
+    commandLineParams()[1]
+  else:
+    ""
+
 proc matchId(s: string): Option[string] =
   s.match(re"\d{8}T\d{6}").map(match)
 
@@ -11,7 +17,9 @@ let allIds = collect:
   for file in walkDir(dir):
     let id = file.path.lastPathPart.matchId
     if id.isSome:
-      id.get
+      if file.path.readFile.find(re(searchTerm)).isSome:
+        id.get
 
-echo allIds
+for id in allIds:
+  echo id
 
