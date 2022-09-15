ebe8703d9e33ec6baa37a83e6897ff0b767c0e2b
Jonas Hvid <mail@johv.dk>
2022-09-15 17:22:57 +0200

    Include file title in output

--- ztf.nim
+++ ztf.nim
@@ -13,12 +13,15 @@ var searchTerm =
 proc matchId(s: string): Option[string] =
   s.match(re"\d{8}T\d{6}").map(match)
 
+proc fileTitle(path: string): string =
+  path.readLines(1)[0]
+
 let allIds = collect:
   for file in walkDir(dir):
     let id = file.path.lastPathPart.matchId
     if id.isSome:
       if file.path.readFile.find(re(searchTerm)).isSome:
-        id.get
+        id.get & " " & file.path.fileTitle
 
 for id in allIds:
   echo id
