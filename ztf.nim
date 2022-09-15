dc2a55f355fde6a892abf0472fce464925b950e1
Jonas Hvid <mail@johv.dk>
2022-09-15 17:45:16 +0200

    Use path for title if no better match is found

--- ztf.nim
+++ ztf.nim
@@ -15,20 +15,28 @@ proc matchId(s: string): Option[string] =
   s.match(re"\d{8}T\d{6}").map(match)
 
 proc fileTitle(path: string): string =
+  let pathTitle = path.find(re"\d{4} (.+)\.")
+
   if path.endsWith(".md"):
     let content = path.readFile
 
-    let h1 = content.find(re"# (.+)")
+    let h1 = content.find(re"(?m)^# (.+)")
     if h1.isSome:
       return h1.get.captures[0]
 
-    let title = content.find(re"title: (.+)(?s:.)+?---")
+    let title = content.find(re"(?m)^\s*title: (.+)(?s:.)+?---")
     if title.isSome:
       return title.get.captures[0]
+
+    if pathTitle.isSome:
+      return pathTitle.get.captures[0]
 
     let afterFrontmatter = content.find(re"---\n(?s:\s)+(.+)")
     if afterFrontmatter.isSome:
       return afterFrontmatter.get.captures[0]
+
+  if pathTitle.isSome:
+    return pathTitle.get.captures[0]
 
   return path.readLines(1)[0]
 
