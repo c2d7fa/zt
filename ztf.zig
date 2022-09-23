936e7bf03a550dada9c4102eebeace9255b78e64
Jonas Hvid <mail@johv.dk>
2022-09-23 15:24:39 +0200

    Add support for title parsing of more formats to Zig ztf

--- ztf.zig
+++ ztf.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 
-fn readTitle(buffer: []u8, file: std.fs.File) anyerror![]u8 {
+fn readTitle(buffer: []u8, file: std.fs.File, name: []const u8) ![]const u8 {
   var len = try file.read(buffer);
   if (len >= buffer.len) len = buffer.len - 1;
+
+  var bestFound: u8 = 0;
 
   var inFrontmatter = false;
   var start: u64 = 0;
@@ -23,6 +25,49 @@ fn readTitle(buffer: []u8, file: std.fs.File) anyerror![]u8 {
       continue;
     }
 
+    if (inFrontmatter and std.mem.startsWith(u8, buffer[i..], "title: ")) {
+      inFrontmatter = false;
+      i += 7;
+      start = i;
+      bestFound = 2;
+      continue;
+    }
+
+    if (!inFrontmatter and std.mem.startsWith(u8, buffer[i..], "\n# ")) {
+      i += 3;
+      start = i;
+      bestFound = 1;
+      continue;
+    }
+
+    if (i == 0 and std.mem.startsWith(u8, buffer[i..], "# ")) {
+      i += 2;
+      start = i;
+      bestFound = 1;
+      continue;
+    }
+
+    if (std.mem.startsWith(u8, buffer[i..], "\n* ")) {
+      i += 3;
+      start = i;
+      bestFound = 1;
+      continue;
+    }
+
+    if (i == 0 and std.mem.startsWith(u8, buffer, "* ")) {
+      i += 2;
+      start = i;
+      bestFound = 1;
+      continue;
+    }
+
+    if (std.mem.startsWith(u8, buffer[i..], "#+TITLE: ") or std.mem.startsWith(u8, buffer[i..], "#+title: ")) {
+      i += 9;
+      start = i;
+      bestFound = 2;
+      continue;
+    }
+
     if (!inFrontmatter and buffer[i] == '\n') {
       if (i == start) {
         i += 1;
@@ -36,7 +81,31 @@ fn readTitle(buffer: []u8, file: std.fs.File) anyerror![]u8 {
     i += 1;
   }
 
+  if (bestFound == 0) {
+    var nameEnd: u64 = name.len - 1;
+
+    while (nameEnd > 0) {
+      if (nameEnd <= 17) {
+        return buffer[start..end];
+      }
+      if (name[nameEnd] == '.') {
+        nameEnd = nameEnd;
+        break;
+      }
+      nameEnd -= 1;
+    }
+
+    return name[16..nameEnd];
+  }
+
   return buffer[start..end];
+}
+
+const InvalidIdError = error { InvalidId };
+
+pub fn parseId(name: []const u8) anyerror![]const u8 {
+  if (name.len < 17) return InvalidIdError.InvalidId;
+  return name[0..15];
 }
 
 pub fn main() !void {
@@ -60,8 +129,9 @@ pub fn main() !void {
     if (entry.kind != std.fs.Dir.Entry.Kind.File) continue;
     var file = try dir.openFile(entry.name, .{});
     defer file.close();
-    const title = try readTitle(&buffer, file);
-    try stdout.print("{s}\n", .{title});
+    const title = try readTitle(&buffer, file, entry.name);
+    const id = parseId(entry.name) catch { continue; };
+    try stdout.print("{s} {s}\n", .{id, title});
   }
 }
 
