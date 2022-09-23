c31d3e3e3f931d62bbf83bc87e946fce205037a2
Jonas Hvid <mail@johv.dk>
2022-09-23 13:55:58 +0200

    Add Zig program that just lists files in directory

--- ztf.zig
+++ ztf.zig
@@ -1,7 +1,22 @@
 const std = @import("std");
 
 pub fn main() !void {
+  var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+  defer arena.deinit();
+  var allocator = arena.allocator();
+
   const stdout = std.io.getStdOut().writer();
-  try stdout.print("Hello, {s}!\n", .{"world"});
+
+  const args = try std.process.argsAlloc(allocator);
+  const dirPath = args[1];
+
+  const path = try std.fs.path.resolve(allocator, &.{dirPath});
+  var dir = try std.fs.openDirAbsolute(path, .{.iterate = true});
+  defer dir.close();
+
+  var iterator = dir.iterate();
+  while (try iterator.next()) |entry| {
+    try stdout.print("{s}\n", .{entry.name});
+  }
 }
 
