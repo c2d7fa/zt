402659ee5d6a716389fe38e65adb094e1e924658
Jonas Hvid <mail@johv.dk>
2022-09-23 14:21:52 +0200

    In the Zig program, print the first line

--- ztf.zig
+++ ztf.zig
@@ -1,5 +1,15 @@
 const std = @import("std");
 
+fn readTitle(buffer: []u8, file: std.fs.File) std.os.ReadError![]u8 {
+  _ = try file.read(buffer);
+
+  for (buffer) |*char, i| {
+    if (char.* == '\n') return buffer[0..i];
+  }
+
+  return buffer[0..1024];
+}
+
 pub fn main() !void {
   var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
   defer arena.deinit();
@@ -14,9 +24,15 @@ pub fn main() !void {
   var dir = try std.fs.openDirAbsolute(path, .{.iterate = true});
   defer dir.close();
 
+  var buffer: [1024]u8 = [_]u8{0} ** 1024;
+
   var iterator = dir.iterate();
   while (try iterator.next()) |entry| {
-    try stdout.print("{s}\n", .{entry.name});
+    if (entry.kind != std.fs.Dir.Entry.Kind.File) continue;
+    var file = try dir.openFile(entry.name, .{});
+    defer file.close();
+    const title = try readTitle(&buffer, file);
+    try stdout.print("{s}\n", .{title});
   }
 }
 
