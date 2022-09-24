34112c2e37c6903d74f431a89c10efbf2b10dff1
Jonas Hvid <mail@johv.dk>
2022-09-24 14:54:56 +0200

    Update dependency on s.el

--- zt.el
+++ zt.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/c2d7fa/zt
 ;; Created: 22 Sep 2022
 ;; Version: 1.0.0
-;; Package-Requires: ((emacs "26.0") (s "0.5"))
+;; Package-Requires: ((emacs "26.0") (s "1.13.1"))
 
 ;;; Code:
 
